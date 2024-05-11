6d8f9e944717b1dfa57681bd3ea53daccdc96f3d
Suresh Chand <scthakuri12a@gmail.com>
2024-05-11 13:15:53 +0545

    Initial

--- App.tsx
+++ App.tsx
@@ -1,118 +1,173 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { Image, ImageBackground, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { OneSignal } from 'react-native-onesignal'
+import Modal from "react-native-modal";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default function App() {
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+    const [visible, setVisible] = useState<boolean>(false);
+    const [notiData, setNotiData] = useState<any>(null)
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+    const getStatuscolor = (status : string) => {
+        try {
+            if(status == "minor"){
+                return "rgba(21, 128, 61, 1)"
+            }else if(status == "mediocre"){
+                return "rgba(180, 83, 9, 1)"
+            }else if(status == "severe"){
+                return "rgba(185, 28, 28, 1)"
+            }
+        } catch (error) {
+            return "#fff"
+        }
+    }
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+    const getStatusbgcolor = (status : string) => {
+        try {
+            if(status == "minor"){
+                return "rgba(220, 252, 231, 1)"
+            }else if(status == "mediocre"){
+                return "rgba(254, 243, 199, 1)"
+            }else if(status == "severe"){
+                return "rgba(254, 226, 226, 1)"
+            }
+        } catch (error) {
+            return "#000"
+        }
+    }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+    useEffect(() => {
+        OneSignal.initialize("b45d498d-4c33-417e-b842-d6009d3873cd")
+        OneSignal.Notifications.requestPermission(true);
+        OneSignal.Notifications.addEventListener('click', (event) => {
+            const additionalData = event.notification.additionalData;
+            if (additionalData){
+                setVisible(true)
+                setNotiData(additionalData)
+            }
+        });
+    }, [])
+    
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    return (
+        <View style={{
+            flex : 1
+        }}>
+            <StatusBar backgroundColor="rgba(0, 0, 0, 1)" />
+            <Modal
+                isVisible={visible}
+                onBackdropPress={() => setVisible(false)}
+            >
+                <View style={{ 
+                    backgroundColor: 'rgba(17, 24, 39, 0.75)',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    borderRadius: 10,
+                    padding: 35
+                }}>
+                    <View style={{
+                        display:"flex",
+                        flexDirection:"row",
+                        alignItems:"center",
+                        justifyContent:"center",
+                        backgroundColor:"rgba(254, 226, 226, 1)",
+                        padding:10,
+                        borderRadius:100
+                    }}>
+                        <Image
+                            source={require("./src/components/file.png")}
+                            style={{
+                                width: 50,
+                                height: 50
+                            }}
+                        />
+                    </View>
+                    
+                    <Text style={{
+                        fontSize:27,
+                        fontWeight:"bold",
+                        color:"#fff",
+                        marginTop:10
+                    }}>Incident Alert</Text>
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+                    <Text style={{
+                        fontSize:18,
+                        color:"#fff",
+                        marginTop:0
+                    }}>There has been an incident.</Text>
+
+                    {
+                        notiData &&
+                        <View style={{
+                            backgroundColor:getStatusbgcolor(notiData.status),
+                            borderRadius:5,
+                            display:"flex",
+                            alignItems:"center",
+                            justifyContent:"center",
+                            paddingVertical:5,
+                            marginTop:20,
+                            paddingHorizontal:10
+                        }}>
+                            <Text style={{
+                                fontSize:17,
+                                color:getStatuscolor(notiData.status),
+                                textTransform:"capitalize"
+                            }}>Status: <Text style={{
+                                fontWeight:"bold"
+                            }}>{notiData.status}</Text></Text>
+                        </View>
+                    }
+
+                    <TouchableOpacity style={{
+                        backgroundColor:"rgba(220, 38, 38, 1)",
+                        borderRadius:5,
+                        display:"flex",
+                        alignItems:"center",
+                        justifyContent:"center",
+                        paddingVertical:10,
+                        marginTop:30,
+                        paddingHorizontal:10
+                    }}>
+                        <Text style={{
+                            color:"#fff",
+                            fontWeight:"bold"
+                        }}>Check Details</Text>
+                    </TouchableOpacity>
+                </View>
+            </Modal>
+
+            <ImageBackground
+                source={require("./src/components/back.jpeg")}
+                style={{
+                    flex : 1,
+                    display:"flex",
+                    alignItems:"center",
+                    justifyContent:"center"
+                }}
+                blurRadius={10}
+                resizeMode="cover"
+            >
+                <Image
+                    source={require("./src/components/logo.png")}
+                    style={{
+                        width:150,
+                        height:150,
+                        marginBottom:20
+                    }}
+                />
+                <Text style={{
+                    color:"#fff",
+                    fontWeight:"bold",
+                    fontSize:25
+                }}>ANAMOLY DETECTION</Text>
+                <Text style={{
+                    color:"#999",
+                    fontWeight:"500",
+                    fontSize:17
+                }}>NYMNA</Text>
+            </ImageBackground>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    )
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+const styles = StyleSheet.create({})
