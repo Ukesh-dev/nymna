dc7f0371d6f3e0f7afdd49a6a74c9c2f501d94e0
Suresh Chand <scthakuri12a@gmail.com>
2024-05-11 07:26:54 +0545

    Fix CORS

--- backend/core/models.py
+++ backend/core/models.py
@@ -4,6 +4,19 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Report(models.Model):
+    """
+    A model representing a report.
+
+    Attributes:
+        STATUS_CHOICES (list): A list of tuples representing the possible statuses for a report.
+        source (models.TextField): The source of the report.
+        confidence (models.FloatField): The confidence level of the report, validated to be between 0 and 1.
+        status (models.CharField): The status of the report, chosen from the STATUS_CHOICES list.
+        timestamp_start (models.IntegerField): The start timestamp of the report, optional.
+        timestamp_end (models.IntegerField): The end timestamp of the report, optional.
+        video (models.CharField): The video associated with the report, optional.
+    """
+
     STATUS_CHOICES = [
         ('minor', 'Minor'),
         ('mediocre', 'Mediocre'),
@@ -21,3 +34,4 @@ class Report(models.Model):
     status = models.CharField(max_length=255, blank=True, null=True, choices=STATUS_CHOICES)
     timestamp_start = models.IntegerField(null=True, blank=True)
     timestamp_end = models.IntegerField(null=True, blank=True)
+    video = models.CharField(max_length=255, blank=True, null=True)
