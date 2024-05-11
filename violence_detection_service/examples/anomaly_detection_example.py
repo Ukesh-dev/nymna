6aa66cd69cb32c86f505c5b27c6a39f1a7b531c9
Suresh Chand <scthakuri12a@gmail.com>
2024-05-11 15:32:56 +0545

    Video Stream Added

--- violence_detection_service/examples/anomaly_detection_example.py
+++ violence_detection_service/examples/anomaly_detection_example.py
@@ -1,7 +1,7 @@
 from violence_detection_service.anomaly_detection.anomaly_detection import AnomalyDetection
 
 if __name__ == '__main__':
-    source_url = "/Users/anishkamukherjee/Documents/nymna/violence_detection_service/videos/person4.mp4"
+    source_url = "http://127.0.0.1:8000/video"
     anomaly_detection = AnomalyDetection(source_url)
     anomaly_detection.get_frame_trims(conf=0.5)
 
