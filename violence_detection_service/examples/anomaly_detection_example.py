ced6d778dafcbc0071d7505b45fd524a75cb30cb
anishka07 <anishkamukherjee237@gmail.com>
2024-05-11 13:27:06 +0545

    update

--- violence_detection_service/examples/anomaly_detection_example.py
+++ violence_detection_service/examples/anomaly_detection_example.py
@@ -1,30 +1,7 @@
-<<<<<<< HEAD
-=======
-import random
-
->>>>>>> 15dda5fc54ae47dfc1ea5cd92631e85e74ac6742
-import cv2
-
 from violence_detection_service.anomaly_detection.anomaly_detection import AnomalyDetection
 
-<<<<<<< HEAD
 if __name__ == '__main__':
     source_url = "/Users/anishkamukherjee/Documents/nymna/violence_detection_service/videos/person4.mp4"
     anomaly_detection = AnomalyDetection(source_url)
     anomaly_detection.get_frame_trims(conf=0.5)
-=======
 
-if __name__ == '__main__':
-    video_url = ""
-    anomaly_detection = AnomalyDetection(video_url)
-
-    cap = cv2.VideoCapture(video_url)
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        results = anomaly_detection.detect_anomaly(frame)
-        if not results:
-            print("No anomalies detected")
-        # for result in results:
->>>>>>> 15dda5fc54ae47dfc1ea5cd92631e85e74ac6742
